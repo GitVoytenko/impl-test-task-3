0ce34726e65247d7099de1b28fc97b3cb00692d8
Dmitriy Voytenko <mailvoytenko@gmail.com>
2022-06-16 16:59:18 +0300

    added the comments

--- src.main.java.com.github.gitvoytenko.impltesttask3/Main.java
+++ src.main.java.com.github.gitvoytenko.impltesttask3/Main.java
@@ -3,11 +3,24 @@ package src.main.java.com.github.gitvoytenko.impltesttask3;
 import java.math.BigInteger;
 
 public class Main {
+
+    /**
+     * Recursive method for calculating the factorial of a number
+     *
+     * @param f is a value from which we will extract the factorial
+     * @return the result of multiplying all values returned by recursion to the values from which recursion is called
+     */
     public static BigInteger getFactorial(int f){
         if(f <= 1) return BigInteger.valueOf(1);
         return BigInteger.valueOf(f).multiply(getFactorial(f - 1));
     }
 
+    /**
+     * Method for summing the digits from the factorial of a number
+     *
+     * @param value is a factorial of the number
+     * @return the sum of all digits from the factorial of the number
+     */
     public static int sumOfFactorial(BigInteger value){
         String str = value.toString();
         int sum = 0;
@@ -17,9 +30,8 @@ public class Main {
         return sum;
     }
     public static void main(String[] args) {
-        BigInteger f = getFactorial(100);
+        BigInteger f = getFactorial(100); // should use the BigInteger class, because the result can be a very large number of
 
-        System.out.println(f);
         System.out.println(sumOfFactorial(f));
     }
 }
